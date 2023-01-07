341a4b2980024a603075eba4499fe95da5094fea
Sangki Kim <goodki283@gmail.com>
2023-01-07 16:41:47 +0900

    send nickname

--- src/server.js
+++ src/server.js
@@ -15,15 +15,17 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app); // httpサーバーにアクセス
 const wss = new WebSocket.Server({ server }); // httpサーバーにwsサーバーを生成
 
-wss.on("connection", (socket) => {
+const sockets = []; // fake database : 接続があったすべてのsocketを格納
+
+wss.on("connection", (socket) => { // frontendからconnectionがあったときに実行
+    sockets.push(socket);
     console.log("Connected to Browser ✅");
     socket.on("close", ()=> {
         console.log("Disconnected from the Browser X");
     })
-    socket.on("message", (messsage) => {
-        console.log(messsage.toString('utf8'));
+    socket.on("message", (message) => {
+        sockets.forEach(aSocket => aSocket.send(message.toString()));
     })
-    socket.send("hello!!!");
-}); // frontendからconnectionがあったときに実行
+}); 
 
 server.listen(3000, handleListen); // localhost:3000ではhttp,ws両方受け取れる
