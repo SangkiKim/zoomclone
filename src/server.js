ceb6b92b6e2758849b98e47148f69966b6adf5ba
Sangki Kim <goodki283@gmail.com>
2023-01-08 17:58:15 +0900

    get all open rooms

--- src/server.js
+++ src/server.js
@@ -15,6 +15,18 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const httpServer = http.createServer(app); // httpサーバーにアクセス
 const wsServer = SocketIO(httpServer);
 
+function publicRooms(){
+    const sids = wsServer.sockets.adapter.sids;
+    const rooms = wsServer.sockets.adapter.rooms;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if(sids.get(key) === undefined){
+            publicRooms.push(key);
+        }
+    })
+    return publicRooms;
+}
+
 wsServer.on("connection", (socket) => {
     socket["nickname"] = "Anon";
     socket.onAny((event) => {
@@ -23,11 +35,15 @@ wsServer.on("connection", (socket) => {
     socket.on("enter_room", (roomName,done) => {
         socket.join(roomName); // roomNameを持ったroomにjoin
         done();
-        socket.to(roomName).emit("welcome", socket.nickname); // (event name)
+        socket.to(roomName).emit("welcome", socket.nickname); // (event name, msg) 特定roomにのみメッセージを送る
+        wsServer.sockets.emit("room_change", publicRooms()); // すべてのroomにメッセージを送る
     });
     socket.on("disconnecting", () => {
         socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname));
     });
+    socket.on("disconnect", ()=>{
+        wsServer.sockets.emit("room_change", publicRooms()); // すべてのroomにメッセージを送る
+    })
     socket.on("new_message", (msg, room, done) => {
         socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
         done();
