4bbecd2e730758e1cb90bda9b24557d7ccd1aa08
Sangki Kim <goodki283@gmail.com>
2023-01-08 11:11:35 +0900

    socket.io first time

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -12,29 +12,38 @@ app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-const server = http.createServer(app); // httpサーバーにアクセス
-const wss = new WebSocket.Server({ server }); // httpサーバーにwsサーバーを生成
+const httpServer = http.createServer(app); // httpサーバーにアクセス
+const wsServer = SocketIO(httpServer);
 
-const sockets = []; // fake database : 接続があったすべてのsocketを格納
+wsServer.on("connection", socket => {
+    socket.on("enter_room", (msg, done) => {
+        console.log(msg);
+        setTimeout(() => {
+            done();
+        }, 10000);
+    });
+});
 
-wss.on("connection", (socket) => { // frontendからconnectionがあったときに実行
-    sockets.push(socket);
-    socket["nickname"] = "Anon";
-    console.log("Connected to Browser ✅");
-    socket.on("close", ()=> {
-        console.log("Disconnected from the Browser X");
-    })
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch(message.type){
-            case "new_message":
-                sockets.forEach(aSocket => aSocket.send(`${socket.nickname} : ${message.payload.toString()}`));
-                break;
-            case "nickname":
-                socket["nickname"] = message.payload.toString();
-                break;
-        }
-    })
-}); 
+// const sockets = []; // fake database : 接続があったすべてのsocketを格納
 
-server.listen(3000, handleListen); // localhost:3000ではhttp,ws両方受け取れる
+// wss.on("connection", (socket) => { // frontendからconnectionがあったときに実行
+//     sockets.push(socket);
+//     socket["nickname"] = "Anon";
+//     console.log("Connected to Browser ✅");
+//     socket.on("close", ()=> {
+//         console.log("Disconnected from the Browser X");
+//     })
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg);
+//         switch(message.type){
+//             case "new_message":
+//                 sockets.forEach(aSocket => aSocket.send(`${socket.nickname} : ${message.payload.toString()}`));
+//                 break;
+//             case "nickname":
+//                 socket["nickname"] = message.payload.toString();
+//                 break;
+//         }
+//     })
+// }); 
+
+httpServer.listen(3000, handleListen); // localhost:3000ではhttp,ws両方受け取れる
